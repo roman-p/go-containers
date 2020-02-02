0d933ae0f72ff76ac603d258f41c0623b148f7d3
Roman Pogribnyi <roman.pogribnyi@gmail.com>
2020-02-02 21:00:44 +0100

    Invoke child process for setting a namespace

--- main.go
+++ main.go
@@ -11,12 +11,27 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("bad comand")
 	}
 }
 
 func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	syscall.Sethostname([]byte("container"))
+ 
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"} s.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
+}
+
+func child() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
@@ -29,6 +44,8 @@ func run() {
 	}
 
 	cmd.Run()
+
+	syscall.Sethostname([]byte("container"))
 }
 
 func must(err error) {
