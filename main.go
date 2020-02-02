7de6cc16ac252aa6e6fc6ca2b730e0028e54c581
Roman Pogribnyi <roman.pogribnyi@gmail.com>
2020-02-02 21:01:45 +0100

    Fix syntax

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func run() {
 
 	syscall.Sethostname([]byte("container"))
  
-	cmd := exec.Command("/proc/self/exe", append([]string{"child"} s.Args[2:]...)...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"} os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
